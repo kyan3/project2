3fd6aa3cb979f774a8ab5d055a3617944a0affb9
Hidra <hantar@hawk.iit.edu>
2017-12-01 01:46:08 -0600

    Reorganized js files

---
+++ js/validate.js
@@ -0,0 +1,18 @@
+function validate()
+      {
+      
+         if( document.myForm.user.value == "" )
+         {
+            alert( "Please provide your Email!" );
+            document.myForm.user.focus() ;
+            return false;
+         }
+               
+         if( document.myForm.password.value == "" )
+         {
+            alert( "Please enter your Password!" );
+            document.myForm.password.focus() ;
+            return false;
+         }
+         return( true );
+      }
